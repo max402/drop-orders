6b13a2c8d72184b11325f8a7c9d9e27e6a2645da
max <grischenko.maxim@gmail.com>
2018-06-03 20:40:12 +0300

    added cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,22 +13,52 @@ export class AppComponent implements OnInit {
 
   areas: Area[] = [];
   cities: City[] = [];
+  areaCities: City[] = [];
+
+  isLoadedAreas = false;
+  isLoadedCities = false;
 
   constructor(private npService: NpService) {}
 
   ngOnInit() {
+    this.isLoadedAreas = false;
     this.npService.getAreas()
       .subscribe(areas => {
         areas.data.map(area => {
           console.log(area);
           this.areas.push(area);
         });
+        this.isLoadedAreas = true;
+      });
+
+    this.isLoadedCities = false;
+    this.npService.getCities()
+      .subscribe(cities => {
+        cities.data.map(city => {
+          console.log(city);
+          this.cities.push(city);
+        });
+        this.isLoadedCities = true;
       });
   }
 
   onChangeArea(newValue) {
-    // this.npService.getCities()
+    this.isLoadedCities = false;
+    this.areaCities = this.cities.filter(c => c.Area === newValue);
+    console.log(newValue);
+    console.log(this.areaCities.length);
+    console.log(this.cities.length);
     console.log('on change area event:' + newValue);
+    this.isLoadedCities = true;
+  }
+
+  onChangeCity(newValue) {
+    //
+    console.log('on change city:' + newValue);
+  }
+
+  trackByIdx(Ref: number, obj: any): any {
+    return Ref;
   }
 
 }
